436060b1695ccea67fddbd59fc89e19dc05db453
acophen <setsio@live.fr>
2018-06-02 00:27:25 +0200

    début de réflexion

--- exo1Parti2.php
+++ exo1Parti2.php
@@ -1,34 +1,21 @@
 <?php
-class Hotel{
+class Hotel {
 	private $nom;
 	private $capacite;
-	private $chambres=array('4116,15,Est','4117,15,Nord','4118,30,Sud');
+	private $chambres=array("4116,15,Est","4117,15,Nord","4118,30,Sud");
 	
-	
-	public function __construct($nom,$capacite){
-		$this->nom=$nom;
-		$this->chambdispo=$capacite;
-		$this->chambreserv= $capacite *0;
-		$this->chambreserv1=3;
-		$this->chambdispo1 -=$capacite;
+	public function __construct ($nom, $capacite){
+		$this ->nom=$nom;
+		$this ->capacite=$capacite;
 	}
-	public function affiche(){
-		echo "le nom de l'hotel: ".$this->nom."<br/>";
-		echo "le nombre de chambres reservées dans l'hôtel: ".$this->chambreserv."<br/>";
-		echo "le nombre de chambres disponibles: ".$this->chambdispo."<br/>";
-		echo "le détail des réservations : <br/>";
-		echo" Aucune réservation!  <br/>";
-		echo"-----------------------<br/>";
+	public function getNom() {
+		return $this->nom;
 	}
-	public function afficher(){
-		echo "le nom de l'hotel: ".$this->nom."<br/>";
-		echo "le nombre de chambres reservées dans l'hôtel: ".$this->chambreserv1."<br/>";
-		echo "le nombre de chambres disponibles: ".$this->chambdispo1."<br/>";
-		echo "le détail des réservations : <br/>";
-		
-}
-$hotelBordeaux= new Hotel("Hôtel Bordeaux", 50);
-echo $hotelBordeaux->affiche();
-echo $hotelBordeaux->afficher();
-	
+	public function getCapacite (){
+		return $this->capacite;
+	}
+	public function ChambreDisponible (){
+		if (isset($this->chambres[0])|| isset($this->chambres[1])|| isset($this->chambres[2]){
+			return $this ->chambrDispo= $capacite;
+		}
 ?>
